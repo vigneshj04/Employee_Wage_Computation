2d856ef4a3f6355197615ec6a02c260036ed7954
vigneshj04 <vigneshjmax@gmail.com>
2022-04-01 00:28:57 +0530

    Added: Employee Wage Per Day

--- EmployeePay.java
+++ EmployeePay.java
@@ -3,6 +3,8 @@ package com.employeewage;
 public class EmployeePay {
 
     static int present = 1;
+    static int Wage_Per_Hour = 20;
+    static int Total_Working_Hrs = 8;
 
     static void attendanceOfEmployee() {
         double empCheck = Math.floor((Math.random() * 10) % 2);
@@ -13,6 +15,12 @@ public class EmployeePay {
         }
     }
 
+    static void wageOfEmployee() {
+        double empCheck = Math.floor((Math.random() * 10) % 1 + 1);
+        if (present == empCheck) {
+            System.out.println("Employee wage per day = " + Total_Working_Hrs * Wage_Per_Hour);
+        }
+    }
 
 }
 
--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 package com.employeewage;
 
+import java.util.Scanner;
+
 import static com.employeewage.EmployeePay.attendanceOfEmployee;
+import static com.employeewage.EmployeePay.wageOfEmployee;
 
 public class Main {
     public static void main(String[] args) {
-        attendanceOfEmployee();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1.Employee Attendance");
+        System.out.println("2.Wage per day for the Employee");
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                attendanceOfEmployee();
+                break;
+            case 2:
+                wageOfEmployee();
+                break;
+
+        }
     }
 }
