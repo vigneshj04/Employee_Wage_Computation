bb0962cc94a39d6ca5b5b0348a38abb8cd3d27f9
vigneshj04 <vigneshjmax@gmail.com>
2022-04-01 00:22:59 +0530

    Added: Employee Attendance

--- EmployeePay.java
+++ EmployeePay.java
@@ -2,6 +2,18 @@ package com.employeewage;
 
 public class EmployeePay {
 
+    static int present = 1;
+
+    static void attendanceOfEmployee() {
+        double empCheck = Math.floor((Math.random() * 10) % 2);
+        if (empCheck == present) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+
+
 }
 
 
--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 package com.employeewage;
 
+import static com.employeewage.EmployeePay.attendanceOfEmployee;
+
 public class Main {
     public static void main(String[] args) {
-
+        attendanceOfEmployee();
     }
 }
